131172e753cb95855a6020882c9f18fe754ab91a
JACK <11111@qq.com>
2015-11-29 12:08:57 +0800

    dfadf
    dfdsf

---
+++ 我的项目/sdfsd/sdfsd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sdfsd
+//
+//  Created by jack on 15/11/29.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
